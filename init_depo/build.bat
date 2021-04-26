dfa0fc2459854e617c6c4004a0905ac3a0f9fc90
morley-stuff <morleyj.mail@gmail.com>
2021-04-26 02:14:45 +0200

    Updated datetime/stamp in windows build to be more specific

--- init_depo/build.bat
+++ init_depo/build.bat
@@ -4,5 +4,5 @@ type src\file1.txt > bin\output.txt || exit 1
 echo "Building file 2..."
 type src\file2.txt >> bin\output.txt || exit 1
 echo "Stamping date..."
-echo %date% >> bin\output.txt || exit 1
+echo %date%-%time% >> bin\output.txt || exit 1
 echo "Build completed."
